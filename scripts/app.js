772472e4b03a75bd24422f992e9c5e5fab66eccd
MrMacchew <Macchew2@gmail.com>
2017-01-16 16:37:52 -0700

    Added some style sheets and scripts.

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,18 @@ for(var x = 0; x < 100; x++)
     for(var z = 0; z < 10; z++)
     {
         var player = {};
-        //player["summonerName"] = match.participantIdentities[0].player.summonerName;
+        player["summonerName"] = matches[x].participantIdentities[z].player.summonerName;
         //player["sid"] = match.participantIdentities[0].player.summonerId;
-        //var pid = match.participantIdentities[0].player.participantId;
-        player["pentakills"] = matches[x].participants[1].stats["pentaKills"];
-        console.log(player["pentakills"]);
-        players.push(player);
+        player["pid"] = matches[x].participantIdentities[z].participantId;
+        player["cid"] = matches[x].participants[z].championId;
+        player["pentakills"] = matches[x].participants[z].stats["pentaKills"];
+        if(player.pentakills > 0){
+            console.log(player["pentakills"] + " | " + player["pid"] + " | " + player["summonerName"] + " | " + getChampById(player.cid));
+        }
+        //players.push(player);
     }
 }
+var apiKey = "RGAPI-06a172cb-cf1f-4769-a269-06cf491f494b";
+var nameHeader = "https://na.api.pvp.name";
+var test = $.getJSON();
+
